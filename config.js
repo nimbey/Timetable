63526639aa5aec08e60579834a03624eaab22199
nimbey <83806046+nimbey@users.noreply.github.com>
2024-11-12 04:39:16 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const config = {
-    API_URL: 'https://your-backend-name.onrender.com'  // You'll get this URL after deploying backend
+    const API_BASE_URL = 'https://timetable-backend-9rbu.onrender.com';
 }; 
