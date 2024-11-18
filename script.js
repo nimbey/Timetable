7e863b5b612c1b56556cadf34202b38fdf40ef12
nimbey <83806046+nimbey@users.noreply.github.com>
2024-11-19 01:35:23 +1100

    Update script.js
    render changes

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     try {
         console.log('Attempting login with:', email);
 
-        const response = await fetch('http://localhost:3001/api/auth/login', {
+        const response = await fetch('https://timetablebackend-a7n9.onrender.com/api/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
